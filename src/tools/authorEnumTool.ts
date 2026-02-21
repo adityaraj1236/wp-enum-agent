0e17d7705c04eb5b6778b6290a4734644ed5a3ea
Aditya Raj <adityaraj06072003@gmail.com>
2026-02-21 22:19:53 +0530

    cleaned code

--- src/tools/authorEnumTool.ts
+++ src/tools/authorEnumTool.ts
@@ -11,49 +11,6 @@ export const authorEnumTool = createTool({
     url: z.string().describe("Base URL of the WordPress target")
   }),
 
-//   execute: async ({ url }) => {
-//     try {
-//       const cleanUrl = url.replace(/\/$/, "");
-//       const endpoint = `${cleanUrl}/?author=1`;
-
-//       const response = await httpClient.get(endpoint, {
-//         maxRedirects: 0,
-//         validateStatus: () => true
-//       });
-
-//       if (!response.success) {
-//         return {
-//           success: false,
-//           message: "Network error during author enumeration"
-//         };
-//       }
-//       const location =
-//         (response as any)?.headers?.location || "";
-
-//       if (location && location.includes("/author/")) {
-//         const username = location.split("/author/")[1].split("/")[0];
-
-//         return {
-//           success: true,
-//           technique: "AUTHOR_ARCHIVE",
-//           username
-//         };
-//       }
-
-//       return {
-//         success: false,
-//         message: "No author redirect detected"
-//       };
-
-//     } catch (error: any) {
-//       return {
-//         success: false,
-//         message: "Unexpected error during author enumeration",
-//         error: error.message
-//       };
-//     }
-//   }
-
 execute: async ({ url }) => {
   try {
     const cleanUrl = url.replace(/\/$/, "");
